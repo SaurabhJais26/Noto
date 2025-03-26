4c881bbe496c3f18169fc4b70b3c4698f668f9cb
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-03-26 12:51:47 +0530

    Initial Commit

---
+++ Noto/NotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotoApp.swift
+//  Noto
+//
+//  Created by Saurabh Jaiswal on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
